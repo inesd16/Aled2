4ca98c3bb07311627aa9c537408b090ee5d8fd85
Ines DOBOSZ <ines.dobosz@isen.yncrea.fr>
2020-04-21 15:59:50 +0200

    Design + Optimized call emergency

--- app/src/main/java/fr/isen/dobosz/projet/MainActivity.kt
+++ app/src/main/java/fr/isen/dobosz/projet/MainActivity.kt
@@ -1,17 +1,13 @@
 package fr.isen.dobosz.projet
 
 import android.annotation.SuppressLint
-import android.content.Context
 import android.content.Intent
-import android.content.SharedPreferences
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.*
+import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
-
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
-import androidx.core.view.isVisible
 import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -52,24 +48,14 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     @SuppressLint("ResourceType")
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         if(item.getItemId() == R.id.nav_connect){
-
-            //setContentView(R.layout.activity_login)
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
         when(item.getItemId()){
-           // R.id.nav_connect -> supportFragmentManager.beginTransaction().replace(R.layout.activity_login,MessageFragment()).commit()
             R.id.nav_connect -> intent = Intent(this, LoginActivity::class.java)
             R.id.nav_sign_in -> intent = Intent(this, RegistrationActivity::class.java)
-
-
-            //R.id.nav_sign_in -> setContentView(R.layout.activity_registration)
-            //R.id.nav_sign_in -> supportFragmentManager.beginTransaction().replace(R.layout.activity_registration,ProfileFragment()).commit()
-
             R.id.nav_contact -> supportFragmentManager.beginTransaction().replace(R.id.fragment_container,LoginFragment()).commit()
             R.id.nav_about_us -> supportFragmentManager.beginTransaction().replace(R.id.fragment_container,LoginFragment()).commit()
-            //R.id.nav_contact -> Toast.makeText(this,"share", Toast.LENGTH_SHORT).show()
-            //R.id.nav_client -> Toast.makeText(this,"send", Toast.LENGTH_SHORT).show()
 
         }
         startActivity(intent)
@@ -84,11 +70,5 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         } else
         super.onBackPressed()
     }
-
-    //override fun onCreateOptionsMenu(menu: Menu): Boolean {
-      //  val inflater: MenuInflater = menuInflater
-        //inflater.inflate(R.menu.drawer_menu, menu)
-        //return true
-   // }
 
 }
